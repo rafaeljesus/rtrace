6f9f76dd3a877c1df14bad67ac99817ca4529311
Rafael Jesus <rafaelljesus86@gmail.com>
2016-06-18 21:17:24 -0300

    Add specs for trace method

---
+++ test/rtrace/event_test.rb
@@ -0,0 +1,14 @@
+require_relative '../test_helper'
+require 'rtrace'
+
+describe 'Rtrace::Event' do
+  after { Rtrace::Event.delete_all }
+
+  describe '.trace' do
+    it 'should trace event' do
+      payload = { name: 'order_created' }
+      p = Rtrace::Event.trace(payload).execute
+      refute_nil p.value
+    end
+  end
+end
